272fee69bf2d790a81e57663361143d221f8e9e5
Sauravmahajan26 <sauravm9822@gmail.com>
2024-09-12 04:25:46 +0530

    font changed to ovo and outfit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sideMenu = document.querySelector("#sideMenu");
+const navBar = document.querySelector("nav");
+const navLink = document.querySelector("nav ul");
 
 function openMenu() {
   sideMenu.style.transform = "translateX(-16rem)"; // Open menu by sliding into view
@@ -7,3 +9,23 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.transform = "translateX(16rem)"; // Close menu by sliding out of view
 }
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLink.classList.remove("bg-white", "shadow-sm", "bg-opacity-50");
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLink.classList.add("bg-white", "shadow-sm", "bg-opacity-50");
+  }
+});
